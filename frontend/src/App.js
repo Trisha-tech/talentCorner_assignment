9f1b11eb251829a53bfc186ce6dc4ccdf303791e
Trisha-tech <1234trishasahu@gmail.com>
2023-04-19 18:20:30 +0530

    Vertical Tab Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes as Switch, Route } from "react-router-dom";
 import MainPage from './components/MainPage';
 import HomePage from './allPages/HomePage';
+import VerticalTabs from './allPages/VerticalTabs';
 
 function App() {
   return (
     <MainPage>
       <Switch>
         <Route path="/" element={<HomePage />} />
+        <Route path="/tab" element={<VerticalTabs />} />
       </Switch>
     </MainPage>
   );
---
+++ frontend/src/allPages/VerticalTabs.js
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100%' }}
+    >
+      <Tabs
+        orientation="vertical"
+        
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab style={{padding:25,textTransform:'capitalize', fontSize:"30px",fontFamily:"Roboto"}} label="Create Workshop" {...a11yProps(0)} />
+        <Tab style={{padding:25,textTransform:'capitalize', fontSize:"30px",fontFamily:"Roboto"}} label="Display Workshops" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Create Workshop
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Display Workshops
+      </TabPanel>
+    </Box>
+  );
+}
