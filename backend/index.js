71c00477770fdf2af1a0a8845be3718d04bf57a7
Trisha-tech <1234trishasahu@gmail.com>
2023-04-19 17:17:54 +0530

    MongoDB Connection Added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
 const express = require(`express`);
 const app = express();
 const PORT =  8080;
+const dotenv = require(`dotenv`)
+const mongoose = require('mongoose')
+
+dotenv.config({path : `.env`})
+
+/*MONGODB CONNECTION START*/
+const MONGO_URL = process.env.MONGO_URL ;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log("Connected to MongoDB")
+})
+mongoose.connection.on('error', (err) => {
+    console.log("Error Connecting to Database", err)
+})
+/*MONGODB CONNECTION END*/
 
 app.use(express.json());
 
